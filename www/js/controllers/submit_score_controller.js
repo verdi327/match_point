9cd03206de4c290857223057c6e117247a4b6ad5
Michael Verdi <mverdi@newrelic.com>
2015-01-27 19:18:34 -0800

    initial pass at the submit score functionality

--- www/js/controllers/submit_score_controller.js
+++ www/js/controllers/submit_score_controller.js
@@ -1,5 +1,58 @@
 var app = angular.module("app");
 
-app.controller('SubmitScoreCtrl', function($scope) {
+app.controller('SubmitScoreCtrl', function($scope, Friends, $filter) {
+  currentUser = {name: "Michael Verdi", photo: "http://placehold.it/50x50", id: 10}
+  $scope.matchResult = {
+    players: [],
+    score: [],
+    date: ""
+  };
+
+  $scope.players = Friends.all();
+
+  buildSets = function(){
+    $scope.matchResult.score = [];
+    _($scope.sets).times(function(n){
+      n++
+      var result = {}
+      result["set"] = n
+      result[$scope.currentUserId] = ""
+      result[$scope.oppId] = ""
+      $scope.matchResult.score.push(result);
+    });
+  };
+
+  $scope.setSets = function(number) {
+    $scope.sets = number;
+    buildSets();
+  };
+
+  $scope.isActive = function(number) {
+    return number === $scope.sets;
+  };
+
+  $scope.selectOpponent = function(player){
+    $scope.selectedOpponent = player;
+    $scope.matchResult.players = [];
+    $scope.oppId = "p" + player.id;
+    $scope.currentUserId = "p" + currentUser.id;
+    $scope.matchResult.players.push($scope.oppId);
+    $scope.matchResult.players.push($scope.currentUserId);
+  };
+
+  $scope.showObj = function() {
+    console.log($scope.matchResult);
+  };
+
+  $scope.formatSetScore = function() {
+    var score = ""
+    _.each($scope.matchResult.score, function(set){
+      var cleaned = _.omit(set, "set", "$$hashKey")
+      var stringified = _.values(cleaned).join("-");
+      score = score + (stringified + " ");
+    });
+    return score;
+  }
+
 
 })
