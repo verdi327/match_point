8d7f554704e8f69433952addd83d9f379b19b109
Michael Verdi <mverdi@newrelic.com>
2015-02-09 17:36:39 -0800

    using individual date and time fields then merging them together, tried incorporating calendar, but it's failing

--- www/js/controllers/calendar_controller.js
+++ www/js/controllers/calendar_controller.js
@@ -1,6 +1,6 @@
 var app = angular.module("app");
 
-app.controller('CalendarCtrl', function($scope, $ionicModal, $timeout, $cordovaDatePicker, $ionicPlatform) {
+app.controller('CalendarCtrl', function($scope, $ionicModal, $timeout, $cordovaCalendar, $ionicPlatform) {
 
   $ionicModal.fromTemplateUrl('templates/modals/calendar.html', {
     scope: $scope,
@@ -9,7 +9,7 @@ app.controller('CalendarCtrl', function($scope, $ionicModal, $timeout, $cordovaD
     $scope.modal = modal;
   });
 
-  $scope.data = {title: "", location: "", notes: "", startDateTime: "", endDateTime: ""};
+  $scope.data = {title: "", location: "", notes: "", startTime: "", endTime: "", date: ""};
 
   $scope.openCalendarModal = function(playerName) {
     $scope.playerName = playerName;
@@ -20,30 +20,43 @@ app.controller('CalendarCtrl', function($scope, $ionicModal, $timeout, $cordovaD
     $scope.modal.hide();
   };
 
+  setTime = function(hours, minutes) {
+    var date  = $scope.data.date;
+    date.setHours(hours);
+    date.setMinutes(minutes);
+    return date
+  }
+
+  calcStartAndEndTime = function() {
+    var start = $scope.data.startTime;
+    var end   = $scope.data.endTime;
+
+    $scope.data.startDateTime = setTime(start.getHours(), start.getMinutes())
+    $scope.data.endDateTime = setTime(end.getHours(), end.getMinutes())
+  }
+
   $scope.addToCalendar = function() {
     $timeout(function() {
+      calcStartAndEndTime()
+      console.log($scope.data);
       $scope.closeModal();
     }, 1000);
   };
 
-  $scope.datePicker = function(){
-    var options = {
-      date: new Date(),
-      mode: 'datetime',
-      minDate: new Date(),
-      allowOldDates: false,
-      allowFutureDates: true,
-      doneButtonLabel: 'DONE',
-      doneButtonColor: '#F2F3F4',
-      cancelButtonLabel: 'CANCEL',
-      cancelButtonColor: '#000000'
-    };
+  // $ionicPlatform.ready(function() {
+  //   $cordovaCalendar.createEvent({
+  //      title: 'Space Race',
+  //      location: 'The Moon',
+  //      notes: 'Bring sandwiches',
+  //      startDate: new Date(2015, 0, 6, 18, 30, 0, 0, 0),
+  //      endDate: new Date(2015, 1, 6, 12, 0, 0, 0, 0)
+  //    }).then(function (result) {
+  //      // success
+  //    }, function (err) {
+  //      // error
+  //    });
+  // });
 
-    $ionicPlatform.ready(function() {
-      $cordovaDatePicker.show(options).then(function(date){
-        alert(date);
-      });
-    });
-  }
+  
 
 })
